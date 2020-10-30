0fc153cf904166ae1bfc11c30aac1ccd9e2ecf85
s@lamandra <ussashok09@gmail.com>
2020-10-30 15:46:49 -0700

    add spec for Semigroup

--- src/main/scala/monoid/Semigroup.scala
+++ src/main/scala/monoid/Semigroup.scala
@@ -5,6 +5,11 @@ trait Semigroup[T] {
 }
 
 object Semigroup {
+
+  implicit class SemigroupFun[T: Semigroup](a: T) {
+    def |+|(b: T): T = implicitly(Semigroup[T]).combine(a, b)
+  }
+
   def apply[T](implicit e: Semigroup[T]): Semigroup[T] = e
 
 
@@ -13,6 +18,8 @@ object Semigroup {
   implicit val semigroupString: Semigroup[String] = (a: String, b: String) => a + b
 
   implicit def semigroupSeq[T]: Semigroup[Seq[T]] = (a: Seq[T], b: Seq[T]) => a ++ b
+
+  implicit def semigroupList[T]: Semigroup[List[T]] = (a: List[T], b: List[T]) => a ++ b
 
   private def combineOpt[T: Semigroup](a: T, bOpt: Option[T]): T = {
     bOpt match {
@@ -30,4 +37,13 @@ object Semigroup {
 
   implicit def semigroupTuple2[T1: Semigroup, T2: Semigroup]: Semigroup[(T1, T2)] = (a: (T1, T2), b: (T1, T2)) => implicitly(Semigroup[T1]).combine(a._1, b._1) -> implicitly(Semigroup[T2]).combine(a._2, b._2)
 
+  implicit def semigroupFun1[T: Semigroup]: Semigroup[T => T] = (a: T => T, b: T => T) => a.andThen(b)
+
+  implicit def semigroupMap[T1, T2](implicit e2: Semigroup[T2]): Semigroup[Map[T1, T2]] = (a: Map[T1, T2], b: Map[T1, T2]) => {
+    a.foldLeft(b) { case (resMap, (key, value)) => resMap.get(key) match {
+      case Some(valueThat) => resMap.updated(key, e2.combine(value, valueThat))
+      case None => resMap
+    }
+    }
+  }
 }
---
+++ src/test/scala/SemigroupSpec.scala
@@ -0,0 +1,42 @@
+
+
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest._
+import matchers._
+
+
+//import cats.Monoid
+//import cats.implicits._
+
+class SemigroupSpec extends AnyFlatSpec with should.Matchers {
+
+  it should "test from https://www.scala-exercises.org/cats/semigroup" in {
+    import monoid.Semigroup
+    import monoid.Semigroup._
+
+    Semigroup[Int].combine(1, 2) should be(3)
+    Semigroup[List[Int]].combine(List(1, 2, 3), List(4, 5, 6)) should be(List(1, 2, 3, 4, 5, 6))
+    Semigroup[Option[Int]].combine(Option(1), Option(2)) should be(Option(3))
+    Semigroup[Option[Int]].combine(Option(1), None) should be(Option(1))
+
+
+    Semigroup[Int => Int].combine(_ + 1, _ * 10).apply(6) should be(70)
+
+
+    val aMap = Map("foo" -> Map("bar" -> 5))
+    val anotherMap = Map("foo" -> Map("bar" -> 6))
+    val combinedMap = Semigroup[Map[String, Map[String, Int]]].combine(aMap, anotherMap)
+    combinedMap.get("foo") should be(Some(Map("bar" -> 11)))
+
+
+    val one: Option[Int] = Option(1)
+    val two: Option[Int] = Option(2)
+    val n: Option[Int] = None
+
+    one |+| two should be(Option(3))
+    n |+| two should be(Option(2))
+    n |+| n should be(None)
+    two |+| n should be(Option(2))
+  }
+}
