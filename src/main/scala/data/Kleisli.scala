d771e2ba61c1e7dbe5475c62a0d8bab3a171b6ce
s@lamandra <ussashok09@gmail.com>
2021-01-23 11:24:01 -0800

    add kleisli

---
+++ src/main/scala/data/Kleisli.scala
@@ -0,0 +1,26 @@
+package data
+
+import monoid.Semigroup
+
+
+case class Kleisli[C, -A, +B](run: A => (B, C)) {
+  def map[D](f: B => D): Kleisli[C, A, D] = Kleisli { a: A =>
+    val (left, right) = run(a)
+    f(left) -> right
+  }
+
+  def flatMap[D](f: B => Kleisli[C, B, D])(implicit semigroup: Semigroup[C]): Kleisli[C, A, D] =
+    Kleisli { a: A =>
+      val (left1, right1) = run(a)
+      val (left2, right2) = f(left1).run(left1)
+      val right = semigroup.combine(right1, right2)
+      left2 -> right
+    }
+}
+
+object Kleisli {
+  def empty[A, B](run: A => B): Kleisli[String, A, B] = Kleisli { x: A =>
+    run(x) -> ""
+  }
+
+}
