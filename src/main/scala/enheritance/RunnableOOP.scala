80153698412208bf5f785717f9a07e85a799ef6a
s@lamandra <ussashok09@gmail.com>
2021-02-20 04:36:06 -0800

    refactoring remove case

--- src/main/scala/enheritance/RunnableOOP.scala
+++ src/main/scala/enheritance/RunnableOOP.scala
@@ -8,14 +8,14 @@ case class Water(volume: Int)
 
 case class Coffee(grade: String)
 
-case object KettleOOP extends RunnableOOP[Water] {
+object KettleOOP extends RunnableOOP[Water] {
   override def run(t: Water): String = s"The water is warming up, volume = ${t.volume} "
 }
 
-case object WaterFilterOOP extends RunnableOOP[Water] {
+object WaterFilterOOP extends RunnableOOP[Water] {
   override def run(t: Water): String = s"FilterWater, volume = ${t.volume} "
 }
 
-case object CoffeeMakerOOP extends RunnableOOP[Coffee] {
+object CoffeeMakerOOP extends RunnableOOP[Coffee] {
   override def run(t: Coffee): String = s"Make coffee, coffee grade = ${t.grade} "
 }
