373f6eb21a70bc1d7b33247239e2c88e7dae21a2
s@lamandra <ussashok09@gmail.com>
2021-03-06 07:46:48 -0800

    fix Foldable

--- src/main/scala/monoid/Foldable.scala
+++ src/main/scala/monoid/Foldable.scala
@@ -3,13 +3,15 @@ package monoid
 import data.{Branch, Leaf, Tree}
 
 trait Foldable[F[_]] {
-  def foldRight[A, B](fa: F[A])(zero: B)(f: (A, B) => B): B
+  def foldRight[A, B](fa: F[A], zero: B)(f: (A, B) => B): B
 
-  def foldLeft[A, B](fa: F[A])(zero: B)(f: (B, A) => B): B
+  def foldLeft[A, B](fa: F[A], zero: B)(f: (B, A) => B): B
+
+  def fold[A](fa: F[A])(implicit S: Semigroup[A]): A
 
   def foldMap[A, B](fa: F[A])(f: A => B)(implicit mb: Monoid[B]): B
 
-  def concatenate[A](fa: F[A])(implicit ma: Monoid[A]): A = foldLeft(fa)(ma.empty)(ma.combine)
+  def concatenate[A](fa: F[A])(implicit ma: Monoid[A]): A = foldLeft(fa, ma.empty)(ma.combine)
 
   //EXERCISE 10.15
   def toList[A](fa: F[A]): List[A]
@@ -22,15 +24,21 @@ object Foldable extends FoldableInstance {
 trait FoldableInstance {
   //  EXERCISE 10.12
   implicit val foldableList: Foldable[List] = new Foldable[List] {
-    override def foldRight[A, B](fa: List[A])(zero: B)(f: (A, B) => B): B = fa match {
-      case head :: tail => f(head, foldRight(tail)(zero)(f))
+    override def foldRight[A, B](fa: List[A], zero: B)(f: (A, B) => B): B = fa match {
+      case head :: tail => f(head, foldRight(tail, zero)(f))
       case Nil => zero
     }
 
-    override def foldLeft[A, B](fa: List[A])(zero: B)(f: (B, A) => B): B = fa match {
-      case head :: tail => f(foldLeft(tail)(zero)(f), head)
+
+    override def foldLeft[A, B](fa: List[A], zero: B)(f: (B, A) => B): B = foldLeftPrivate(fa.reverse, zero)(f)
+
+    private def foldLeftPrivate[A, B](fa: List[A], zero: B)(f: (B, A) => B): B = fa match {
+      case head :: tail => f(foldLeftPrivate(tail, zero)(f), head)
       case Nil => zero
     }
+
+
+    override def fold[A](fa: List[A])(implicit S: Semigroup[A]): A = fa.reduce(S.combine)
 
     override def foldMap[A, B](fa: List[A])(f: A => B)(implicit mb: Monoid[B]): B = fa.foldLeft(mb.empty) { case (l, r) => mb.combine(l, f(r)) }
 
@@ -38,9 +46,12 @@ trait FoldableInstance {
   }
 
   implicit val foldableSeq: Foldable[Seq] = new Foldable[Seq] {
-    override def foldRight[A, B](fa: Seq[A])(zero: B)(f: (A, B) => B): B = fa.foldRight(zero)(f)
+    override def foldRight[A, B](fa: Seq[A], zero: B)(f: (A, B) => B): B = fa.foldRight(zero)(f)
 
-    override def foldLeft[A, B](fa: Seq[A])(zero: B)(f: (B, A) => B): B = fa.foldLeft(zero)(f)
+    override def foldLeft[A, B](fa: Seq[A], zero: B)(f: (B, A) => B): B = fa.foldLeft(zero)(f)
+
+
+    override def fold[A](fa: Seq[A])(implicit S: Semigroup[A]): A = fa.reduce(S.combine)
 
     override def foldMap[A, B](fa: Seq[A])(f: A => B)(implicit mb: Monoid[B]): B = fa.map(f).fold(mb.empty)(mb.combine)
 
@@ -49,16 +60,21 @@ trait FoldableInstance {
 
   //    EXERCISE 10.13
   implicit val foldableTree: Foldable[Tree] = new Foldable[Tree] {
-    override def foldRight[A, B](fa: Tree[A])(zero: B)(f: (A, B) => B): B = fa match {
+    override def foldRight[A, B](fa: Tree[A], zero: B)(f: (A, B) => B): B = fa match {
       case Leaf(value) => f(value, zero)
-      case Branch(left, right) => foldRight(left)(foldRight(right)(zero)(f))(f)
+      case Branch(left, right) => foldRight(left, foldRight(right, zero)(f))(f)
     }
 
-    override def foldLeft[A, B](fa: Tree[A])(zero: B)(f: (B, A) => B): B = fa match {
+    override def foldLeft[A, B](fa: Tree[A], zero: B)(f: (B, A) => B): B = fa match {
       case Leaf(value) => f(zero, value)
-      case Branch(left, right) => foldLeft(right)(foldLeft(left)(zero)(f))(f)
+      case Branch(left, right) => foldLeft(left, foldLeft(right, zero)(f))(f)
     }
 
+
+    override def fold[A](fa: Tree[A])(implicit S: Semigroup[A]): A = fa match {
+      case Leaf(value) => value
+      case Branch(left, right) => S.combine(fold(left), fold(right))
+    }
 
     override def foldMap[A, B](fa: Tree[A])(f: A => B)(implicit mb: Monoid[B]): B = fa match {
       case Leaf(value) => f(value)
@@ -73,15 +89,17 @@ trait FoldableInstance {
 
   //  EXERCISE 10.14
   implicit val foldableOption: Foldable[Option] = new Foldable[Option] {
-    override def foldRight[A, B](fa: Option[A])(zero: B)(f: (A, B) => B): B = fa match {
+    override def foldRight[A, B](fa: Option[A], zero: B)(f: (A, B) => B): B = fa match {
       case Some(value) => f(value, zero)
       case None => zero
     }
 
-    override def foldLeft[A, B](fa: Option[A])(zero: B)(f: (B, A) => B): B = fa match {
+    override def foldLeft[A, B](fa: Option[A], zero: B)(f: (B, A) => B): B = fa match {
       case Some(value) => f(zero, value)
       case None => zero
     }
+
+    override def fold[A](fa: Option[A])(implicit S: Semigroup[A]): A = fa.get
 
     override def foldMap[A, B](fa: Option[A])(f: A => B)(implicit mb: Monoid[B]): B = {
       fa match {
---
+++ src/test/scala/monoid/FoldableSpec.scala
@@ -0,0 +1,97 @@
+package monoid
+
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers._
+
+class FoldableSpec extends AnyFlatSpec with should.Matchers {
+
+  import monoid.Foldable._
+
+
+  it should "check diffrent types" in {
+    //    Monoid[Int].combine(1, 2) shouldBe 3
+    //    Monoid[String].combine("1", "2") shouldBe "12"
+    //    Monoid[Seq[Int]].combine(Seq(1), Seq(2)) shouldBe Seq(1, 2)
+    //    Monoid[List[Int]].combine(List(1, 2), List(3, 4)) shouldBe Seq(1, 2, 3, 4)
+    //    Monoid[Seq[Seq[Int]]].combine(Seq(Seq(1), Seq(3)), Seq(Seq(2))) shouldBe Seq(Seq(1), Seq(3), Seq(2))
+    //    Monoid[Option[Int]].combine(Some(1), Some(2)) shouldBe Some(3)
+    //    Monoid[Option[Int]].combine(Some(1), None) shouldBe Some(1)
+    //    Monoid[Option[Int]].combine(None, Some(2)) shouldBe Some(2)
+    //    Monoid[Option[Int]].combine(None, None) shouldBe None
+    //    Monoid[Seq[Option[Int]]].combine(Seq(Some(1)), Seq(Some(2))) shouldBe Seq(Some(1), Some(2))
+    //    Monoid[(Int, String)].combine(1 -> "1", 2 -> "2") shouldBe 3 -> "12"
+    //    Monoid[Seq[(Int, String)]].combine(Seq(1 -> "1"), Seq(2 -> "2")) shouldBe Seq(1 -> "1", 2 -> "2")
+    //    Monoid.combineAll(Seq("1", "2", "3")) shouldBe "123"
+    //    Monoid.combineAll(Seq(1, 2, 3)) shouldBe 6
+    //
+    //    val l1: IndexedSeq[Int] = List(1, 2, 3, 4, 5).toIndexedSeq
+    //    l1.foldMapV(x => x) should be(15)
+    //    l1.foldMapV(i => i.toString) should be("12345")
+    //    l1.foldMapV(i => (i, i.toString)) should be(15 -> "12345")
+    //    List(1, 2).toIndexedSeq.foldMapV(i => (i, i.toString)) should be(3 -> "12")
+    //
+    //    bag(Vector("a", "rose", "is", "a", "rose")) shouldBe Map("a" -> 2, "rose" -> 2, "is" -> 1)
+  }
+
+  it should "test from https://www.scala-exercises.org/cats/foldable" in {
+    Foldable[List].foldRight(List(1, 2, 3), 0)(_ + _) should be(6)
+    Foldable[List].foldRight(List("a", "b", "c"), "")(_ + _) should be("abc")
+    Foldable[List].foldLeft(List(1, 2, 3), 0)(_ + _) should be(6)
+    Foldable[List].foldLeft(List("a", "b", "c"), "")(_ + _) should be("abc")
+
+
+    //    val lazyResult =
+    //      Foldable[List].foldRight(List(1, 2, 3), Now(0))((x, rest) => Later(x + rest.value))
+    //    lazyResult.value should be()
+
+    Foldable[List].fold(List("a", "b", "c")) should be("abc")
+    Foldable[List].fold(List(1, 2, 3)) should be(6)
+
+    Foldable[List].foldMap(List("a", "b", "c"))(_.length) should be(3)
+    Foldable[List].foldMap(List(1, 2, 3))(_.toString) should be("123")
+
+
+    //    Foldable[List].foldK(List(List(1, 2), List(3, 4, 5))) should be()
+    //    Foldable[List].foldK(List(None, Option("two"), Option("three"))) should be()
+    //
+    //
+//        Foldable[List].find(List(1, 2, 3))(_ > 2) should be()
+//        Foldable[List].find(List(1, 2, 3))(_ > 5) should be()
+    //
+    //    Foldable[List].exists(List(1, 2, 3))(_ > 2) should be()
+    //    Foldable[List].exists(List(1, 2, 3))(_ > 5) should be()
+    //
+    //    Foldable[List].forall(List(1, 2, 3))(_ <= 3) should be()
+    //    Foldable[List].forall(List(1, 2, 3))(_ < 3) should be()
+    //
+    //    Foldable[List].toList(List(1, 2, 3)) should be()
+    //    Foldable[Option].toList(Option(42)) should be()
+    //    Foldable[Option].toList(None) should be()
+    //
+    //
+    //    Foldable[List].filter_(List(1, 2, 3))(_ < 3) should be()
+    //    Foldable[Option].filter_(Option(42))(_ != 42) should be()
+    //
+    //    def parseInt(s: String): Option[Int] =
+    //      Either.catchOnly[NumberFormatException](s.toInt).toOption
+
+    //    traverse the foldable mapping A values to G[B], and combining them using Applicative[G] and discarding the results.
+    //
+    //      This method is primarily useful when G[_] represents an action or effect, and the specific B aspect of G[B] is not otherwise needed. The B will be discarded and Unit returned instead.
+    //      Foldable[List].traverse_(List("1", "2", "3"))(parseInt) should be()
+    //    Foldable[List].traverse_(List("a", "b", "c"))(parseInt) should be()
+
+    //
+    //    val FoldableListOption = Foldable[List].compose[Option]
+    //    FoldableListOption.fold(List(Option(1), Option(2), Option(3), Option(4))) should be()
+    //    FoldableListOption.fold(List(Option("1"), Option("2"), None, Option("3"))) should be()
+    //
+    //
+    //
+    //    Foldable[List].isEmpty(List(1, 2, 3)) should be()
+    //    Foldable[List].dropWhile_(List(1, 2, 3))(_ < 2) should be()
+    //    Foldable[List].takeWhile_(List(1, 2, 3))(_ < 2) should be()
+  }
+
+}
