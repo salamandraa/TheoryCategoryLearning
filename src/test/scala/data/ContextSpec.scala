9d93c7dc42ad32975b42cc32c79a15a70d3c42e6
alex <ussashok09@gmail.com>
2021-03-11 17:18:52 +0300

    fix context

--- src/test/scala/data/ContextSpec.scala
+++ src/test/scala/data/ContextSpec.scala
@@ -10,18 +10,22 @@ object ContextSpec {
 
   sealed trait Context[F[+_]]
 
-  case class ExecutionContextImpl(context: ExecutionContext) extends Context[Future]
-
-  case class ContextShiftIoImpl(context: ContextShift[IO]) extends Context[IO]
-
-  //  case class ContextShiftImpl[F[_]](context: ContextShift[F]) extends Context[F]
-
   object Context {
+    private case class ExecutionContextImpl(context: ExecutionContext) extends Context[Future]
 
+    implicit class ExecutionContextImplOps(context: ExecutionContext) {
+      def toContextFuture: Context[Future] = ExecutionContextImpl(context)
+    }
 
     implicit def toExecutionContext(implicit context: Context[Future]): ExecutionContext = context match {
       case ExecutionContextImpl(context) => context
       case _ => ???
+    }
+
+    private case class ContextShiftIoImpl(context: ContextShift[IO]) extends Context[IO]
+
+    implicit class ContextShiftIoImplOps(context: ContextShift[IO]) {
+      def toContextIO: Context[IO] = ContextShiftIoImpl(context)
     }
 
     implicit def toContextShift(implicit context: Context[IO]): ContextShift[IO] = context match {
@@ -41,14 +45,13 @@ class ContextSpec extends AnyFlatSpec with should.Matchers {
 
     import Context._
 
-    implicit val executionContextImpl: Context[Future] = ExecutionContextImpl(scala.concurrent.ExecutionContext.global)
+    implicit val executionContextImpl: Context[Future] = scala.concurrent.ExecutionContext.global.toContextFuture
 
     def printExecutionContext(implicit executionContext: ExecutionContext): Unit = println(executionContext)
 
     printExecutionContext
 
-
-    implicit val contextShiftIoImpl: Context[IO] = ContextShiftIoImpl(cats.effect.IO.contextShift(scala.concurrent.ExecutionContext.global))
+    implicit val contextShiftIoImpl: Context[IO] = cats.effect.IO.contextShift(scala.concurrent.ExecutionContext.global).toContextIO
 
     def printContextShiftIo(implicit context: ContextShift[IO]): Unit = println(context)
 
